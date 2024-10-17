7dc1c57050c912b2568ae5af8f7cdd51ee467f04
Silvus <hugo.combe.84@gmail.com>
2024-10-18 00:00:20 +0200

    feat : add total tim (hours, mins, secs) on screen

--- src/Page/Screen.tsx
+++ src/Page/Screen.tsx
@@ -1,16 +1,18 @@
 import {useState} from "react";
 import getLs from "../utils/getLocalStorage.ts";
 import setLs from "../utils/setLocalStorage.ts";
-import {showTime} from "../utils/utils.ts";
+import {showTime, showTimeWithoutMinutes} from "../utils/utils.ts";
 
 import "../assets/style/screen.css";
 import countdownBar from "../assets/videos/countdown-bar-green.webm";
+import clsx from "clsx";
 
 export default function Screen() {
     const [counter, setCounter] = useState(0);
     const [workingTime, setWorkingTime] = useState(getLs.WORKING_TIME());
     const [pauseTime, setPauseTime] = useState(getLs.PAUSE_TIME());
     const [isWorkScene, setIsWorkScene] = useState(getLs.IS_WORK_SCENE());
+    const [totalWorkingTime, setTotalWorkingTime] = useState(getLs.TOTAL_TIME());
     setInterval(function () {
         setCounter(getLs.COUNTER);
         if (getLs.REFRESH()) {
@@ -20,16 +22,27 @@ export default function Screen() {
         setWorkingTime(getLs.WORKING_TIME());
         setPauseTime(getLs.PAUSE_TIME());
         setIsWorkScene(getLs.IS_WORK_SCENE());
+        setTotalWorkingTime(getLs.TOTAL_TIME());
     }, 250);
-
     const countdownBarWidth = counter / (isWorkScene ? workingTime*60 : pauseTime*60) * 100;
     return (
       isWorkScene ? (
         <>
-            <div className="recap">
-                <p className="workingTime">{(workingTime<10) ? "0"+workingTime:workingTime}</p>
-                <p className="pauseTime">{pauseTime}</p>
+          <div className="recap">
+            <p className="workingTime">{(workingTime<10) ? "0"+workingTime:workingTime}</p>
+            <p className="pauseTime">{pauseTime}</p>
+          </div>
+          <div className={"totalTimeGroup"}>
+            <div className={clsx("totalTimeScreen",getLs.SHOW_WORKING_TIME_MINUTES()&&"two-lines")}>
+              <p className={"totalTime"}>{showTimeWithoutMinutes(totalWorkingTime)}H</p>
+              <span>
+                {getLs.SHOW_WORKING_TIME_MINUTES() &&
+                  <p className={"totalTime"}>{showTime(totalWorkingTime).split(":")[1]} {getLs.SHOW_WORKING_TIME_SECONDS() ? "M" : "min"}</p>}
+                {getLs.SHOW_WORKING_TIME_SECONDS() && <p className={"totalTime"}>{showTime(totalWorkingTime).split(":")[2]}S</p>}
+              </span>
             </div>
+            <p className={"totalTimeGoal"}>/{getLs.WORKING_TIME_GOAL()}H</p>
+          </div>
             <div className={"countdown"}>
                 <p className={"white"}>{showTime(counter)}</p>
                 <style>
