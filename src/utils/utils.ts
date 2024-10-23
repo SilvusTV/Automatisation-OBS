62646de0f52e7815e21960bce77a92593b5d80ca
Silvus <hugo.combe.84@gmail.com>
2024-10-23 15:46:17 +0200

    fix : show hour if total time is under

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,12 +8,12 @@ export function showTime(time: number) {
   const hours = Math.floor(time / 3600)
   const minutes = Math.floor((time / 60) % 60)
   const seconds = time % 60
-  return (hours > 0 ? hours + ':' : '') + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+  return (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds
 }
 export function showTimeWithoutSeconds(time: number) {
   const hours = Math.floor(time / 3600)
   const minutes = Math.floor((time / 60) % 60)
-  return (hours > 0 ? hours + ':' : '') + (minutes < 10 ? '0' : '') + minutes
+  return (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes
 }
 export function showTimeWithoutMinutes(time: number) {
   return Math.floor(time / 3600)
